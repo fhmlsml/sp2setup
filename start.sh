1b9eb8fc00bf9f4bdb60ef35e1c2ec033f64cc02
fhmlsml <email.gemail10@gmail.com>
2021-05-02 02:32:46 +0700

    hmm

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y nginx s3fs php-fpm php-mysql mysql-client
 # setup pass s3
 sudo echo s3fskey > /etc/passwd-s3fs
 sudo chmod 640 /etc/passwd-s3fs
-sudo mkdir /var/www/pesbuk
+sudo mkdir /var/www/pesbuk/img # since github doesn't store empty directory, so i create it
 sudo cp -R /sp2setup-master/* /var/www/pesbuk
 # sudo s3fs ember-mochidaz /var/www/pesbuk/img -o passwd_file=/etc/passwd-s3fs -o url=https://s3.ap-southeast-1.amazonaws.com -ouid=1001,gid=1001,allow_other
 
@@ -15,7 +15,7 @@ sed -i 's/val_db_pass/db_passwd/g' /var/www/pesbuk/config.php
 sed -i 's/val_db_name/db_name/g' /var/www/pesbuk/config.php
 
 
-#nginx
+# setup nginx sites file
 sudo cp /sp2setup-master/pesbuk.conf /etc/nginx/sites-available
 sudo ln -s /etc/nginx/sites-available/pesbuk.conf /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-available/default
